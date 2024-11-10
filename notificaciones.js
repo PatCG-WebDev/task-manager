b62278c1ad1200527ad0e1b4cdd62948d530780f
PatCG-WebDev <cidgonzalezp@gmail.com>
2024-11-10 21:56:57 +0100

    Finalizo el desarrollo

--- notificaciones.js
+++ notificaciones.js
@@ -1,2 +1,8 @@
 Realización de cambios para hacer el primer commit en notificaciones.
 Esto es un cambio que hago desde la rama notificaciones
+//Esto es una modificacion de la rama "otro desarrollo"
+//Son cambios muy importantes! 
+
+//cambio grande en otro desarrollo
+
+Finalizar cambios de notificaciones.
